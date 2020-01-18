4147bc86073a9d9656b33bf2d454cc3ac2f4eb4f
Giancarlo Deleon <giancarlodeleon@Giancarlos-MacBook-Pro.local>
2020-01-18 18:17:32 -0500

    Updates

---
+++ precache-manifest.c3f4b03bff2849d79cdca1b4830628eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e015356376e4aa6381dc0e1b447d2fc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5c8ce6767a55fe37d587",
+    "url": "/monsters-rolodex/static/css/main.e9167ac3.chunk.css"
+  },
+  {
+    "revision": "7e894316149ac95bff0e",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c8ce6767a55fe37d587",
+    "url": "/monsters-rolodex/static/js/main.ad9e5ed4.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
